52776d5ef2f877589ccb9e67d3ea09fbe22b5620
annisa <ichataraha@gmail.com>
2016-10-12 00:05:06 +0700

    First Design

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2005/formpendaftaranaudisi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl2005/formpendaftaranaudisi/MainActivity.java
@@ -2,12 +2,34 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl2005.formpendaftaranaudisi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etName;
+    EditText etNationality;
+    Button bSubmit;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = (EditText) findViewById(R.id.editTextName);
+        etNationality = (EditText) findViewById(R.id.editTextNationality);
+        bSubmit = (Button) findViewById(R.id.buttonSubmit);
+        tvResult = (TextView) findViewById(R.id.textViewResult);
+
+        bSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                String nationality = etNationality.getText().toString();
+                tvResult.setText("Your name :" + name + "Nationality :" + nationality);
+            }
+        });
     }
 }
